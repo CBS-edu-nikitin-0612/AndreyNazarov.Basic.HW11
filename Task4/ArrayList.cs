3a4abbef499992e2dfcd289688521999d5ef9d06
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-02-09 14:17:17 +0300

    feat: Task4

---
+++ Task4/ArrayList.cs
@@ -0,0 +1,23 @@
+﻿namespace Task4
+{
+    internal class ArrayList
+    {
+        private object[] array;
+        public int Count { get; private set; }
+
+        public void Add(object obj)
+        {
+            array[Count] = obj;
+            Count++;
+        }
+        public ArrayList()
+        {
+            array = new object[10];
+        }
+
+        public object this[int index]
+        {
+            get => array[index];
+        }
+    }
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(1);
+            arrayList.Add("text");
+            arrayList.Add(new ArrayList());
+            arrayList.Add(new object());
+
+            Console.WriteLine(arrayList[0]);
+            Console.WriteLine(arrayList[1]);
+            Console.WriteLine(arrayList[2]);
+            Console.WriteLine(arrayList[3]);
+        }
+    }
+}
