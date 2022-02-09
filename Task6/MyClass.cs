f5a27c90a529ab423933f1b33ffbf28e3d9a8b4b
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-02-09 14:44:10 +0300

    feat:Task6

---
+++ Task6/MyClass.cs
@@ -0,0 +1,11 @@
+﻿namespace Task6
+{
+    internal class MyClass<T> where T : new()
+    {
+        // Тип Т должен иметь доступный конструктор по умолчанию
+        public T FactoryMethod()
+        {
+            return new T();
+        }
+    }
+}
---
+++ Task6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass<Random> myClass = new();
+
+            var rnd = myClass.FactoryMethod();
+
+            Console.WriteLine(rnd.Next());
+        }
+    }
+}
