fab13c7f44688e32deb1356828cf8b7777e3b58d
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-02-09 14:37:11 +0300

    feat: Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Task4;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Может я задание не так понял, но ни с какми проблемами не столкнулся, может имеется ввиду что хранится все это будет как объект?
+            ArrayList arrayList = new();
+            arrayList.Add(1);
+            arrayList.Add(2);
+            arrayList.Add(3);
+            arrayList.Add("text");
+            arrayList.Add(new ArrayList());
+            arrayList.Add(new ArrayList());
+
+
+            for (int i = 0; i < arrayList.Count; i++)
+            {
+                Console.WriteLine(arrayList[i]);
+            }
+        }
+    }
+}
